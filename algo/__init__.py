3a3f84fb52f7660202e20d48226beaeda5089e5e
April <april51292617@gmail.com>
2025-07-31 09:59:54 +0800

    7.31

---
+++ algo/__init__.py
@@ -0,0 +1,1 @@
+# This file makes the algo directory a Python package
---
+++ algo/fedavg/server.py
@@ -0,0 +1,23 @@
+import random
+import torch
+
+def get_clients_this_round(fed_args, round):
+    if (fed_args.fed_alg).startswith('local'):
+        clients_this_round = [int((fed_args.fed_alg)[-1])]
+    else:
+        if fed_args.num_clients < fed_args.sample_clients:
+            clients_this_round = list(range(fed_args.num_clients))
+        else:
+            random.seed(round)
+            clients_this_round = sorted(random.sample(range(fed_args.num_clients), fed_args.sample_clients))
+    return clients_this_round
+
+def global_aggregate(global_dict, local_dict_list, sample_num_list, clients_this_round):
+    sample_this_round = sum([sample_num_list[client] for client in clients_this_round])
+    global_auxiliary = None
+
+   # Normal dataset-size-based aggregation
+    for key in global_dict.keys():
+        global_dict[key] = sum([local_dict_list[client][key] * sample_num_list[client] / sample_this_round for client in clients_this_round])
+       
+    return global_dict, global_auxiliary
