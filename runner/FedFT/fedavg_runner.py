140d0835d2a35f72fe202fff61ead26f2203cbed
April <april51292617@gmail.com>
2025-08-04 15:30:05 +0800

    modify path

--- runner/FedFT/fedavg_runner.py
+++ runner/FedFT/fedavg_runner.py
@@ -16,7 +16,7 @@ from peft import (
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
--- runner/FedFT/local_runner.py
+++ runner/FedFT/local_runner.py
@@ -14,7 +14,7 @@ from peft import (
     prepare_model_for_kbit_training
 )
 
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
